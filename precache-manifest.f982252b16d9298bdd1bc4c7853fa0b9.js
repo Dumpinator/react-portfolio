99b064cd0f29b13299af697bb8e2d99b7a0415c9
Jonathan de BOISVILLIERS <jdeboisvilliers@gmail.com>
2020-08-09 20:47:09 +0200

    Updates

---
+++ precache-manifest.f982252b16d9298bdd1bc4c7853fa0b9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d0f248eaf0c9eeb01e2d297169f7f0",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b522717c3db4ab3cd59f",
+    "url": "/react-portfolio/static/css/main.478bd15b.chunk.css"
+  },
+  {
+    "revision": "c72a738a9a8b33501303",
+    "url": "/react-portfolio/static/js/2.0ab9dcc3.chunk.js"
+  },
+  {
+    "revision": "fdc84dfb581e6fb8755dd1b2b01a50e1",
+    "url": "/react-portfolio/static/js/2.0ab9dcc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b522717c3db4ab3cd59f",
+    "url": "/react-portfolio/static/js/main.4405da24.chunk.js"
+  },
+  {
+    "revision": "0346f5e54be923396aed",
+    "url": "/react-portfolio/static/js/runtime-main.8a7d631b.js"
+  },
+  {
+    "revision": "e1eaa5546c938dc5d70accf0504c41f1",
+    "url": "/react-portfolio/static/media/MonCV.e1eaa554.pdf"
+  },
+  {
+    "revision": "37895fde7aead064c5798eabb2efc5b0",
+    "url": "/react-portfolio/static/media/codecademy.37895fde.png"
+  },
+  {
+    "revision": "2e4e0704a4b7ddb0ee321780a8b3f482",
+    "url": "/react-portfolio/static/media/codewar.2e4e0704.png"
+  },
+  {
+    "revision": "33c6f3d1adf7ba9b221d206e7d2a7695",
+    "url": "/react-portfolio/static/media/sololearn.33c6f3d1.png"
+  },
+  {
+    "revision": "08d5d8221612488b228a924b48114d19",
+    "url": "/react-portfolio/static/media/test.08d5d822.jpg"
+  }
+]);
